c3f0ca4f80ce5a8660b8e7076e5dca9ff188a512
Tim Austin <tja.xpprime@gmail.com>
2022-09-15 09:39:50 -0500

    add basic hardcoded styling

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
+container.setAttribute('id', 'container');
 body.appendChild(container);
 
 const x = 4;
@@ -9,6 +10,7 @@ function createGrid() {
     for (i = 0; i < x; i++) {
         let row = document.createElement('div');
         row.setAttribute('id', `row${i + 1}`)
+        row.setAttribute('class', 'row');
         container.appendChild(row);
     
         for (j = 0; j < y; j++) {
